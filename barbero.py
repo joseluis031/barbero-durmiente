223f756af650458a7b692daef2b3432d990aa6af
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-04-05 12:36:52 +0200

    corrigiendo errores

--- barbero.py
+++ barbero.py
@@ -23,7 +23,7 @@ def barbero():  #funcion del barbero
         silla.acquire()     #el barbero espera a que el cliente se siente
         cortar.release()        #el barbero puede cortar
         barbero = 1     #el barbero esta cortando
-        print("Barbero esta cortando el pelo")      
+        print("Barbero esta cortando el pelo al cliente {0}".format(clientes))      
         time.sleep(2)       #el barbero tarda 2 segundos en cortar
         cortado.release()    #el cliente puede irse
         barbero = 0    #el barbero termino de cortar
@@ -38,20 +38,21 @@ def cliente():  #funcion del cliente
         mutex.acquire()    #para controlar el numero de clientes
         if clientes < 3:    #si hay menos de 3 clientes en la barberia
             clientes += 1   #se suma un cliente
-            print("\nCliente llega a la barberia")
+            print("\nCliente {} llega a la barberia".format(clientes))
             silla.release()    #el cliente se sienta
             mutex.release()   #se libera el mutex
             cortar.acquire()    #el cliente espera a que el barbero lo atienda
-            print("\nCliente esta siendo atendido") 
+            print("\nCliente {} esta siendo atendido".format(clientes)) 
             cortado.acquire()   #el cliente espera a que el barbero termine de cortar
             mutex.acquire()    #se libera el mutex
-            clientes -= 1   #se resta un cliente
-            print("\nCliente se va de la barberia")
+            
+            print("\nCliente {} se va de la barberia con corte nuevo".format(clientes))
             mutex.release()   #se libera el mutex
         else:   #si hay mas de 3 clientes en la barberia
+            clientes += 1   #se suma un cliente
             mutex.release()  #se libera el mutex
-            print("\nCliente se va de la barberia")
-            time.sleep(2)   #el cliente espera 2 segundos y vuelve a intentar entrar
+            print("\nCliente {} se va de la barberia porque esta llena".format(clientes))
+            time.sleep(10)   #el cliente espera  segundos y vuelve a intentar entrar
             
 def main():
     
@@ -63,7 +64,7 @@ def main():
     while True: 
         hilo_cliente = threading.Thread(target=cliente)   #se crea el hilo del cliente
         hilo_cliente.start()    #se inicia el hilo del cliente
-        time.sleep(2)   #el cliente llega cada 2 segundos
+        time.sleep(10)   #el cliente llega cada 10 segundos
 
 if __name__ == "__main__":
     main()
